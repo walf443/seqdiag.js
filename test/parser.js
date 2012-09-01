5d254e6f61568c59289779ab8bac1968007103b9
Keiji, Yoshimi <walf443@gmail.com>
2012-09-01 16:09:40 +0900

    parse graph.

--- test/parser.js
+++ test/parser.js
@@ -1,12 +1,18 @@
 require('./test_helper.js');
 (function() {
     "use strict";
+    var q = QUnit;
 
-    QUnit.test('Parser', function() {
-        Seqdiag.Parser.parse("test");
-        QUnit.ok(1);
+    q.test('Parser', function() {
+        var ast = Seqdiag.Parser.parse("seqdiag {\n" + 
+            "A[label = \"aaa\"];\n" +
+            "B[label = \"bbb\"];\n" +
+        "}");
+        q.ok(ast instanceof Array);
+        q.ok(ast[0] instanceof Array);
+        q.equal(ast[0][0], "Graph");
     });
 
-    QUnit.start();
+    q.start();
 })();
 
