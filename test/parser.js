17df750ca8caaf1c1d3bad81f6b294bd914dd68a
Keiji, Yoshimi <walf443@gmail.com>
2012-09-01 18:33:12 +0900

    attributesをサポートしてやる

--- test/parser.js
+++ test/parser.js
@@ -5,7 +5,7 @@ require('./test_helper.js');
 
     q.test('Parser', function() {
         var ast = Seqdiag.Parser.parse("seqdiag {\n" + 
-            "A[label = \"aaa\"];\n" +
+            "A[label = \"aaa\", foo = \"bar\"];\n" +
             "B[label = \"bbb\"];\n" +
         "}");
 
@@ -21,10 +21,21 @@ require('./test_helper.js');
         q.equal(ast[1]["stmt"][0][0], "node", "first statement should be node");
         q.equal(typeof ast[1]["stmt"][0][1] , "object", "node should have attributes");
         q.equal(ast[1]["stmt"][0][1]["id"], "A", "node id should be A");
+        q.ok(ast[1]["stmt"][0][1]["attributes"] instanceof Array, "node should have attributes");
 
+        // for node A's attribute
+        q.equal(ast[1]["stmt"][0][1]["attributes"][0], "attributes", "it should be attribute token");
+        q.deepEqual(ast[1]["stmt"][0][1]["attributes"][1], { "label": "aaa", "foo": "bar" }, "attirbute values ok");
+
+        // for node B
         q.equal(ast[1]["stmt"][1][0], "node", "first statement should be node");
         q.equal(typeof ast[1]["stmt"][1][1] , "object", "node should have attributes");
         q.equal(ast[1]["stmt"][1][1]["id"], "B", "node id should be B");
+        q.ok(ast[1]["stmt"][1][1]["attributes"] instanceof Array, "node should have attributes");
+
+        // for node B's attribute
+        q.equal(ast[1]["stmt"][1][1]["attributes"][0], "attributes", "it should be attribute token");
+        q.deepEqual(ast[1]["stmt"][1][1]["attributes"][1], { "label": "bbb" }, "attirbute values ok");
     });
 
     q.start();
